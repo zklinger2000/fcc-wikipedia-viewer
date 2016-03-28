b5f1e2c04f07a3db9039d1b90c58440fbd9c05d6
Zack Klinger <zklinger@gmail.com>
2016-03-28 08:34:38 -0700

    UPDATE wiki factory and controller, ADD dependencies

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 // app.js
 angular.module('wikiApp', [])
 
-.controller('mainController', function($scope) {
-  $scope.name = "Zack";
+
+.factory('wikiService', function($http) {
+
+  
+  var wikiService = {
+    get: function(search) {
+      return $http.jsonp('http://en.wikipedia.org/w/api.php?action=query&titles=' + search + '&prop=revisions&rvlimit=max&rvprop=content&list=backlinks&bltitle=' + search + '&bllimit=55&format=json&callback=JSON_CALLBACK');
+    }
+  };
+
+  return wikiService;
+})
+.controller('MainController', function($scope, wikiService, $http) {
+  var vm = this;
+  
+  vm.searchWiki = searchWiki;
+  
+  function searchWiki(search) {
+    wikiService.get(search).then(function(data) {
+      console.log(data);
+      $scope.wikiData = data.data.query;
+    });
+  }
+
 });
